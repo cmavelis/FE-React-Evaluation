e92e2e32a65809018ada73a1e5c10595c71a2906
Cameron Avelis <cmavelis@gmail.com>
2019-11-01 19:23:59 -0400

    login button & fcn placeholder

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,11 +8,16 @@ const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleLogin = () => {
+    console.log(username, password);
+  };
+
   return (
     <div className="login-form">
-      <p>Login to our magic portal</p>
+      <p>Login to Our Magic Portal</p>
       <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button type="button" onClick={handleLogin}>Login</button>
     </div>
   );
 };
